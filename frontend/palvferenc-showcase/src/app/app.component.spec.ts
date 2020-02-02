2ec73b3f9541b4e1cfe50a674417840cb988f537
Ferenc Palvolgyi <palvferenc@gmail.com>
2020-02-02 22:01:40 +0100

    -make user component spec to green

--- frontend/palvferenc-showcase/src/app/app.component.spec.ts
+++ frontend/palvferenc-showcase/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('palvferenc-showcase');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('palvferenc-showcase app is running!');
-  });
 });
--- frontend/palvferenc-showcase/src/app/user/user.component.spec.ts
+++ frontend/palvferenc-showcase/src/app/user/user.component.spec.ts
@@ -1,6 +1,8 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from '../user.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -8,6 +10,10 @@ describe('UserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers:[
+        UserService,
+      ],
       declarations: [ UserComponent ]
     })
     .compileComponents();
